a7068a484e54644ef4644e65e9caa3621d996ce5
Gary Kacmarcik <garykac@gmail.com>
2018-03-20 17:38:05 -0700

    Only include Writing System keys in polyfill

--- keyboard-polyfill.js
+++ keyboard-polyfill.js
@@ -24,10 +24,10 @@ var Keyboard = function() {
 				'Minus': '-',
 				'Equal': '=',
 				'IntlYen': '\\',
-				'Backspace': 'Backspace',
+				//'Backspace': 'Backspace',
 				
 				// 2nd row
-				'Tab': 'Tab',
+				//'Tab': 'Tab',
 				'KeyQ': 'q',
 				'KeyW': 'w',
 				'KeyE': 'e',
@@ -43,7 +43,7 @@ var Keyboard = function() {
 				'Backslash': '\\',
 				
 				// 3rd row
-				'CapsLock': 'CapsLock',
+				//'CapsLock': 'CapsLock',
 				'KeyA': 'a',
 				'KeyS': 's',
 				'KeyD': 'd',
@@ -55,10 +55,10 @@ var Keyboard = function() {
 				'KeyL': 'l',
 				'Semicolon': ':',
 				'Quote': "'",
-				'Enter': 'Enter',
+				//'Enter': 'Enter',
 
 				// 4th row
-				'ShiftLeft': 'Shift',
+				//'ShiftLeft': 'Shift',
 				'IntlBackslash': '\\',
 				'KeyZ': 'z',
 				'KeyX': 'x',
@@ -71,70 +71,19 @@ var Keyboard = function() {
 				'Period': '.',
 				'Slash': '/',
 				//IntlRo
-				'ShiftRight': 'Shift',
+				//'ShiftRight': 'Shift',
 				
 				// 5th row
-				'ControlLeft': 'Control',
-				'MetaLeft': 'Meta',
-				'AltLeft': 'Alt',
-				'Space': ' ',
-				'AltRight': 'Alt',
-				'MetaRight': 'Meta',
+				//'ControlLeft': 'Control',
+				//'MetaLeft': 'Meta',
+				//'AltLeft': 'Alt',
+				//'Space': ' ',
+				//'AltRight': 'Alt',
+				//'MetaRight': 'Meta',
 				//ContextMenu
-				'ControlRight': 'Control',
+				//'ControlRight': 'Control',
 				
-				// Control pad
-				'Insert': 'Insert',
-				'Home': 'Home',
-				'PageUp': 'PageUp',
-				'Delete': 'Delete',
-				'End': 'End',
-				'PageDown': 'PageDown',
-				
-				// Arrow pad
-				'ArrowUp': 'ArrowUp',
-				'ArrowLeft': 'ArrowLeft',
-				'ArrowDown': 'ArrowDown',
-				'ArrowRight': 'ArrowRight',
-
-				// Function keys
-				'Escape': 'Escape',
-				'F1': 'F1',
-				'F2': 'F2',
-				'F3': 'F3',
-				'F4': 'F4',
-				'F5': 'F5',
-				'F6': 'F6',
-				'F7': 'F7',
-				'F8': 'F8',
-				'F9': 'F9',
-				'F10': 'F10',
-				'F11': 'F11',
-				'F12': 'F12',
-				'PrintScreen': 'PrintScreen',
-				'ScrollLock': 'ScrollLock',
-				'Pause': 'Pause',
-				
-				// Numpad
-				'NumLock': 'NumLock',
-				'NumpadDivide': '/',
-				'NumpadMultiply': '*',
-				'NumpadSubtract': '-',
-				'NumpadAdd': '+',
-				'NumpadEnter': 'Enter',
-				'NumpadDecimal': '.',
-				'Numpad0': '0',
-				'Numpad1': '1',
-				'Numpad2': '2',
-				'Numpad3': '3',
-				'Numpad4': '4',
-				'Numpad5': '5',
-				'Numpad6': '6',
-				'Numpad7': '7',
-				'Numpad8': '8',
-				'Numpad9': '9',
-
-				'Unidentified': 'Unidentified',
+				'Unidentified': 'Unidentified'
 			}
 		}
 	};
